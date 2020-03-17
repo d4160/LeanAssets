fa82659acdcf6b1072c67efe254acd64232a59dc
d4160 <d4160@outlook.com>
2020-03-17 09:37:00 -0500

    first commit

---
+++ Pool/PoolableEvents.cs
@@ -0,0 +1,61 @@
+﻿// UltEvents // Copyright 2019 Kybernetik //
+
+using Lean.Pool;
+using UnityEngine;
+
+namespace UltEvents
+{
+    /// <summary>
+    /// Holds <see cref="UltEvent"/>s which are called by various <see cref="MonoBehaviour"/> 2D collision events:
+    /// <see cref="OnCollisionEnter2D"/>, <see cref="OnCollisionStay2D"/>, and <see cref="OnCollisionExit2D"/>.
+    /// </summary>
+    [AddComponentMenu(UltEventUtils.ComponentMenuPrefix + "Poolable Events")]
+    [DisallowMultipleComponent]
+    public class PoolableEvents : MonoBehaviour, IPoolable
+    {
+        /************************************************************************************************************************/
+
+        [SerializeField]
+        private UltEvent _OnSpawnEvent;
+
+        public UltEvent OnSpawnEvent
+        {
+            get
+            {
+                if (_OnSpawnEvent == null)
+                    _OnSpawnEvent = new UltEvent();
+                return _OnSpawnEvent;
+            }
+            set { _OnSpawnEvent = value; }
+        }
+
+        /************************************************************************************************************************/
+
+        [SerializeField]
+        private UltEvent _OnDespawnEvent;
+
+        public UltEvent OnDespawnEvent
+        {
+            get
+            {
+                if (_OnDespawnEvent == null)
+                    _OnDespawnEvent = new UltEvent();
+                return _OnDespawnEvent;
+            }
+            set { _OnDespawnEvent = value; }
+        }
+
+        /************************************************************************************************************************/
+
+
+        public void OnSpawn()
+        {
+            OnSpawnEvent.Invoke();
+        }
+
+        public void OnDespawn()
+        {
+            OnDespawnEvent.Invoke();
+        }
+    }
+}
